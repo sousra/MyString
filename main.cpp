4213154f3006109119b6a67af6ef186c32b5152b
sousra <guliev.eduardrd@mail.ru>
2021-03-07 01:54:33 +0300

    right tabs

--- main.cpp
+++ main.cpp
@@ -6,74 +6,74 @@ using namespace std;
 
 int main()
 {
-    MyString s("word");
-    s.print();
-    cout << "size: " << s.Size() << endl;
-    s.addChar('l', 4);
-    cout << "->" << endl;
-    s.print();
-    cout << "size: " << s.Size() << endl;
+	MyString s("word");
+	s.print();
+	cout << "size: " << s.Size() << endl;
+	s.addChar('l', 4);
+	cout << "->" << endl;
+	s.print();
+	cout << "size: " << s.Size() << endl;
 
-    cout << "----------------------" << endl;
+	cout << "----------------------" << endl;
 
-    MyString s2("house");
-    s2.print();
-    cout << "size: " << s2.Size() << endl;
-    s2.replaceChar('m', 1);
-    cout << "->" << endl;
-    s2.print();
-    cout << "size: " << s2.Size() << endl;
+	MyString s2("house");
+	s2.print();
+	cout << "size: " << s2.Size() << endl;
+	s2.replaceChar('m', 1);
+	cout << "->" << endl;
+	s2.print();
+	cout << "size: " << s2.Size() << endl;
 
-    cout << "----------------------" << endl;
+	cout << "----------------------" << endl;
 
-    MyString s3("world");
-    s3.print();
-    cout << "size: " << s3.Size() << endl;
-    s3.deleteChar(4);
-    cout << "->" << endl;
-    s3.print();
-    cout << "size: " << s3.Size() << endl;
+	MyString s3("world");
+	s3.print();
+	cout << "size: " << s3.Size() << endl;
+	s3.deleteChar(4);
+	cout << "->" << endl;
+	s3.print();
+	cout << "size: " << s3.Size() << endl;
 
-    cout << "----------------------" << endl;
+	cout << "----------------------" << endl;
 
-    MyString s4("Hello, ");
-    s4.print();
-    cout << "size: " << s4.Size() << endl;
-    s4.concatenate(s);
-    cout << "->" << endl;
-    s4.print();
-    cout << "size: " << s4.Size() << endl;
+	MyString s4("Hello, ");
+	s4.print();
+	cout << "size: " << s4.Size() << endl;
+	s4.concatenate(s);
+	cout << "->" << endl;
+	s4.print();
+	cout << "size: " << s4.Size() << endl;
 
-    cout << "----------------------" << endl;
+	cout << "----------------------" << endl;
 
-    MyString s5 = s4;
-    s5.deleteChar(2);
-    s5.print();
-    cout << "size: " << s5.Size() << endl;
-    s4.print();
-    cout << "size: " << s4.Size() << endl;
+	MyString s5 = s4;
+	s5.deleteChar(2);
+	s5.print();
+	cout << "size: " << s5.Size() << endl;
+	s4.print();
+	cout << "size: " << s4.Size() << endl;
 
-    cout << "----------------------" << endl;
+	cout << "----------------------" << endl;
 
-    MyString s6("text");
-    s6 = s4;
-    s6.deleteChar(2);
-    s6.print();
-    cout << "size: " << s6.Size() << endl;
-    s4.print();
-    cout << "size: " << s4.Size() << endl;
+	MyString s6("text");
+	s6 = s4;
+	s6.deleteChar(2);
+	s6.print();
+	cout << "size: " << s6.Size() << endl;
+	s4.print();
+	cout << "size: " << s4.Size() << endl;
 
-    cout << "----------------------" << endl;
+	cout << "----------------------" << endl;
 
-    MyString s7 = "Hello, ", s8 = "world";
-    s7.print();
-    cout << "size: " << s7.Size() << endl;
-    s7 + s8;
-    cout << "->" << endl;
-    s7.print();
-    cout << "size: " << s7.Size() << endl;
+	MyString s7 = "Hello, ", s8 = "world";
+	s7.print();
+	cout << "size: " << s7.Size() << endl;
+	s7 + s8;
+	cout << "->" << endl;
+	s7.print();
+	cout << "size: " << s7.Size() << endl;
 
-    cout << "----------------------" << endl;
+	cout << "----------------------" << endl;
 
 
 
--- myString.h
+++ myString.h
@@ -3,26 +3,26 @@
 
 class MyString {
 public:
-    MyString(const char* s);
-    MyString(const MyString& s);
-    ~MyString();
-    MyString& operator = (const MyString& s);
-    MyString& operator = (const char* s);
-    MyString& operator + (const MyString& s);
-    MyString& operator + (const char* s);
+	MyString(const char* s);
+	MyString(const MyString& s);
+	~MyString();
+	MyString& operator = (const MyString& s);
+	MyString& operator = (const char* s);
+	MyString& operator + (const MyString& s);
+	MyString& operator + (const char* s);
 
-    unsigned Size() const;
-    void resize(unsigned n_size);
-    void print() const;
-    void addChar(char c, unsigned pos);
-    void replaceChar(char c, unsigned pos);
-    void deleteChar(unsigned pos);
-    void concatenate(MyString s);
-    void concatenate(char* s);
+	unsigned Size() const;
+	void resize(unsigned n_size);
+	void print() const;
+	void addChar(char c, unsigned pos);
+	void replaceChar(char c, unsigned pos);
+	void deleteChar(unsigned pos);
+	void concatenate(MyString s);
+	void concatenate(char* s);
 
 private:
-    unsigned size;
-    char* str;
+	unsigned size;
+	char* str;
 };
 
 
